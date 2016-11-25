10a84c5bfda60c77979517a73a905eea352b3a12
HSarode-Compumatrice <hsarode@edrnet.com>
2016-11-25 12:50:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express'),
     app = express();
 
 app.use(express.static('www'));
+// Created directory "newdir"
 
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
 app.all('*', function(req, res, next) {
