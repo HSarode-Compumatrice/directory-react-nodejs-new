3598df19b58302404441389f5a7d776b8a1005a3
HSarode-Compumatrice <hsarode@edrnet.com>
2016-11-25 15:57:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require('express'),
     employees = require('./routes/employees'),
     app = express();
-
 app.use(express.static('www'));
-// Created directory "newdir1" into /var/www/html/
+// Created Directory "newdir1" into /var/www/html/
 
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
 app.all('*', function(req, res, next) {
