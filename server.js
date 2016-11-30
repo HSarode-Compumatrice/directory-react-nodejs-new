3dd9c334f7c33b1138379918e3a92184d2c50962
HSarode-Compumatrice <hsarode@edrnet.com>
2016-11-30 12:17:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var express = require('express'),
     app = express();
 app.use(express.static('www'));
 // Created Directory "directory-react-nodejs-Harshal" into /var/www/html/
-//  Hi from ashish karpe 
-
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
